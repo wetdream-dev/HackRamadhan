a77fd73488a21a31a594eb43640eab847a318324
Administrator <Administrator@DESKTOP-RS4G5F2>
2025-03-07 08:08:41 +0700

    Add project files.

---
+++ HackRamadhan/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace HackRamadhan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fasttime = new System.Windows.Forms.CheckBox();
+            this.autobuka = new System.Windows.Forms.CheckBox();
+            this.autosahur = new System.Windows.Forms.CheckBox();
+            this.hackpahala = new System.Windows.Forms.CheckBox();
+            this.antilapar = new System.Windows.Forms.CheckBox();
+            this.autokhatam = new System.Windows.Forms.CheckBox();
+            this.hackamal = new System.Windows.Forms.CheckBox();
+            this.antinafsu = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblstatus = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Agency FB", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hack Ramadhan 2025";
+            // 
+            // fasttime
+            // 
+            this.fasttime.AutoSize = true;
+            this.fasttime.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fasttime.Location = new System.Drawing.Point(33, 83);
+            this.fasttime.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.fasttime.Name = "fasttime";
+            this.fasttime.Size = new System.Drawing.Size(77, 19);
+            this.fasttime.TabIndex = 1;
+            this.fasttime.Text = "Fast Time";
+            this.fasttime.UseVisualStyleBackColor = true;
+            // 
+            // autobuka
+            // 
+            this.autobuka.AutoSize = true;
+            this.autobuka.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autobuka.Location = new System.Drawing.Point(33, 110);
+            this.autobuka.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.autobuka.Name = "autobuka";
+            this.autobuka.Size = new System.Drawing.Size(81, 19);
+            this.autobuka.TabIndex = 1;
+            this.autobuka.Text = "Auto Buka";
+            this.autobuka.UseVisualStyleBackColor = true;
+            // 
+            // autosahur
+            // 
+            this.autosahur.AutoSize = true;
+            this.autosahur.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autosahur.Location = new System.Drawing.Point(33, 136);
+            this.autosahur.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.autosahur.Name = "autosahur";
+            this.autosahur.Size = new System.Drawing.Size(85, 19);
+            this.autosahur.TabIndex = 1;
+            this.autosahur.Text = "Auto Sahur";
+            this.autosahur.UseVisualStyleBackColor = true;
+            // 
+            // hackpahala
+            // 
+            this.hackpahala.AutoSize = true;
+            this.hackpahala.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hackpahala.Location = new System.Drawing.Point(33, 163);
+            this.hackpahala.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.hackpahala.Name = "hackpahala";
+            this.hackpahala.Size = new System.Drawing.Size(91, 19);
+            this.hackpahala.TabIndex = 1;
+            this.hackpahala.Text = "Hack Pahala";
+            this.hackpahala.UseVisualStyleBackColor = true;
+            // 
+            // antilapar
+            // 
+            this.antilapar.AutoSize = true;
+            this.antilapar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.antilapar.Location = new System.Drawing.Point(195, 83);
+            this.antilapar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.antilapar.Name = "antilapar";
+            this.antilapar.Size = new System.Drawing.Size(80, 19);
+            this.antilapar.TabIndex = 1;
+            this.antilapar.Text = "Anti Lapar";
+            this.antilapar.UseVisualStyleBackColor = true;
+            // 
+            // autokhatam
+            // 
+            this.autokhatam.AutoSize = true;
+            this.autokhatam.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.autokhatam.Location = new System.Drawing.Point(195, 110);
+            this.autokhatam.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.autokhatam.Name = "autokhatam";
+            this.autokhatam.Size = new System.Drawing.Size(96, 19);
+            this.autokhatam.TabIndex = 1;
+            this.autokhatam.Text = "Auto Khatam";
+            this.autokhatam.UseVisualStyleBackColor = true;
+            // 
+            // hackamal
+            // 
+            this.hackamal.AutoSize = true;
+            this.hackamal.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hackamal.Location = new System.Drawing.Point(195, 136);
+            this.hackamal.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.hackamal.Name = "hackamal";
+            this.hackamal.Size = new System.Drawing.Size(84, 19);
+            this.hackamal.TabIndex = 1;
+            this.hackamal.Text = "Hack Amal";
+            this.hackamal.UseVisualStyleBackColor = true;
+            // 
+            // antinafsu
+            // 
+            this.antinafsu.AutoSize = true;
+            this.antinafsu.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.antinafsu.Location = new System.Drawing.Point(195, 163);
+            this.antinafsu.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.antinafsu.Name = "antinafsu";
+            this.antinafsu.Size = new System.Drawing.Size(82, 19);
+            this.antinafsu.TabIndex = 1;
+            this.antinafsu.Text = "Anti Nafsu";
+            this.antinafsu.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 203);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 232);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kota :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(82, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(209, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START HACKING";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(33, 296);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(258, 10);
+            this.progressBar.TabIndex = 5;
+            // 
+            // lblstatus
+            // 
+            this.lblstatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblstatus.Location = new System.Drawing.Point(0, 309);
+            this.lblstatus.Name = "lblstatus";
+            this.lblstatus.Size = new System.Drawing.Size(321, 23);
+            this.lblstatus.TabIndex = 6;
+            this.lblstatus.Text = "Idle...";
+            this.lblstatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 800;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(321, 332);
+            this.Controls.Add(this.lblstatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.antinafsu);
+            this.Controls.Add(this.hackpahala);
+            this.Controls.Add(this.hackamal);
+            this.Controls.Add(this.autosahur);
+            this.Controls.Add(this.autokhatam);
+            this.Controls.Add(this.antilapar);
+            this.Controls.Add(this.autobuka);
+            this.Controls.Add(this.fasttime);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximumSize = new System.Drawing.Size(337, 371);
+            this.MinimumSize = new System.Drawing.Size(337, 371);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hack Ramadhan";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox fasttime;
+        private System.Windows.Forms.CheckBox autobuka;
+        private System.Windows.Forms.CheckBox autosahur;
+        private System.Windows.Forms.CheckBox hackpahala;
+        private System.Windows.Forms.CheckBox antilapar;
+        private System.Windows.Forms.CheckBox autokhatam;
+        private System.Windows.Forms.CheckBox hackamal;
+        private System.Windows.Forms.CheckBox antinafsu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblstatus;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ HackRamadhan/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HackRamadhan
+{
+    public partial class Form1 : Form
+    {
+        private int currentCheckboxIndex = 0;
+        private string[] checkboxStatuses;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkboxStatuses = new string[]
+            {
+                "Fast Time: OFF",
+                "Auto Buka: OFF",
+                "Auto Sahur: OFF",
+                "Hack Pahala: OFF",
+                "Anti Lapar: OFF",
+                "Auto Khatam: OFF",
+                "Hack Amal: OFF",
+                "Anti Nafsu: OFF"
+            };
+
+            timer.Tick += StatusUpdateTimer_Tick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                if (string.IsNullOrEmpty(textBox2.Text))
+                {
+                    MessageBox.Show("Masukan nama dan kota.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                checkboxStatuses[0] = $"Fast Time: {(fasttime.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[1] = $"Auto Buka: {(autobuka.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[2] = $"Auto Sahur: {(autosahur.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[3] = $"Hack Pahala: {(hackpahala.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[4] = $"Anti Lapar: {(antilapar.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[5] = $"Auto Khatam: {(autokhatam.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[6] = $"Hack Amal: {(hackamal.Checked ? "ON" : "OFF")}";
+                checkboxStatuses[7] = $"Anti Nafsu: {(antinafsu.Checked ? "ON" : "OFF")}";
+
+                lblstatus.Text = "Hacking...";
+                currentCheckboxIndex = 0;
+                progressBar.Value = 0;
+                progressBar.Maximum = checkboxStatuses.Length;
+                timer.Start();
+            }
+        }
+
+        private void StatusUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            lblstatus.Text = checkboxStatuses[currentCheckboxIndex];
+            progressBar.Value = currentCheckboxIndex + 1;
+            currentCheckboxIndex++;
+            if (currentCheckboxIndex >= checkboxStatuses.Length)
+            {
+                timer.Stop();
+                string statusFastTime = fasttime.Checked ? "ON" : "OFF";
+                string statusAutoBuka = autobuka.Checked ? "ON" : "OFF";
+                string statusAutoSahur = autosahur.Checked ? "ON" : "OFF";
+                string statusHackPahala = hackpahala.Checked ? "ON" : "OFF";
+                string statusAntiLapar = antilapar.Checked ? "ON" : "OFF";
+                string statusAutoKhatam = autokhatam.Checked ? "ON" : "OFF";
+                string statusHackAmal = hackamal.Checked ? "ON" : "OFF";
+                string statusAntiNafsu = antinafsu.Checked ? "ON" : "OFF";
+                string message = $"Fast Time = {statusFastTime}\nAuto Buka = {statusAutoBuka}\nAuto Sahur= {statusAutoSahur}\nHack Pahala = {statusHackPahala}\n" +
+                                 $"Anti Lapar = {statusAntiLapar}\nAuto Khatam = {statusAutoKhatam}\nHack Amal = {statusHackAmal}\nAnti Nafsu = {statusAntiNafsu}";
+                if (MessageBox.Show(message, "Hack Status", MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                    progressBar.Value = 0;
+                    lblstatus.Text = "Idle...";
+                }
+            }
+        }
+    }
+}
